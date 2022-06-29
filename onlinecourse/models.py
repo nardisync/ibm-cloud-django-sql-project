6312065eb622597543d0d850cfaac8c3e52d38f4
nardisync <nardisync@gmail.com>
2022-06-29 17:07:52 +0000

    Added the 'Start the Exame' button with the right access to the model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -103,7 +103,7 @@ class Enrollment(models.Model):
     # Other fields and methods you would like to design
 class Question(models.Model):
     # Foreign key to lesson
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete = models.CASCADE)
     
     # question text
     question_text = models.TextField(default = "Text of the Question")
